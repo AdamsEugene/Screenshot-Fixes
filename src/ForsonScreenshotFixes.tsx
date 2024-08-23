9310615d093fa683b965e09b6eb5f39793e5b2f8
Isaac Forson <youngtech303@gmail.com>
2024-08-23 13:29:16 +0000

    code refactor

--- src/ForsonScreenshotFixes.tsx
+++ src/ForsonScreenshotFixes.tsx
@@ -14,60 +14,47 @@ export default class ForsonScreenshotFixes {
     this.adjustImageStyles();
     this.removeDisplayNone();
     this.removeProperties();
+    this.applyResponsiveStyles();
   }
 
   private adjustBannerImageHeight(): void {
-    if (this.document.body.clientWidth <= 430) return;
-
-    const bannerImage = this.document.querySelector('.banner_image');
-    if (!(bannerImage instanceof HTMLImageElement)) return;
-
-    const parentElement = bannerImage.parentElement;
-    if (!parentElement) return;
-
-    const parentHeight = parentElement.offsetHeight;
-    bannerImage.style.setProperty('min-height', `${parentHeight}px`);
+    if (this.isDesktopView()) {
+      const bannerImage = this.getElement<HTMLImageElement>('.banner_image');
+      if (bannerImage && bannerImage.parentElement) {
+        bannerImage.style.setProperty('min-height', `${bannerImage.parentElement.offsetHeight}px`);
+      }
+    }
   }
 
   private adjustImages(): void {
-    this.adjustBannerImages();
+    this.adjustElementImages('.banner__media.media', 749);
     this.adjustHoverEffectImages();
   }
 
-  private adjustBannerImages(): void {
-    const viewportWidth = this.document.body.clientWidth;
-    this.document.querySelectorAll('.banner__media.media').forEach(bannerElement => {
-      const bannerImage = bannerElement.querySelector('img');
-      if (!(bannerImage instanceof HTMLImageElement)) return;
+  private adjustPromoGridImages(): void {
+    this.adjustElementImages('.promo-grid__bg', 767);
+  }
 
-      if (viewportWidth > 749) {
-        this.setImageDimensions(bannerImage, bannerElement);
-      } else {
-        this.removeImageDimensions(bannerImage);
+  private adjustElementImages(selector: string, breakpoint: number): void {
+    const viewportWidth = this.document.body.clientWidth;
+    this.getElements(selector).forEach(element => {
+      const image = element.querySelector('img');
+      if (image instanceof HTMLImageElement) {
+        if (viewportWidth > breakpoint) {
+          this.setImageDimensions(image, element);
+        } else {
+          this.removeImageDimensions(image);
+        }
       }
     });
   }
 
   private adjustHoverEffectImages(): void {
-    this.document.querySelectorAll(".media.media--hover-effect").forEach(el => {
+    this.getElements(".media.media--hover-effect").forEach(el => {
       const images = el.querySelectorAll('img');
       const targetImage = images.length >= 2 ? images[1] : images[0];
       if (targetImage instanceof HTMLImageElement) {
         targetImage.style.minHeight = '100%';
-      }
-    });
-  }
-
-  private adjustPromoGridImages(): void {
-    const viewportWidth = this.document.body.clientWidth;
-    this.document.querySelectorAll('.promo-grid__bg').forEach(promoElement => {
-      const promoImage = promoElement.querySelector('img');
-      if (!(promoImage instanceof HTMLImageElement)) return;
-
-      if (viewportWidth > 767) {
-        this.setImageDimensions(promoImage, promoElement);
-      } else {
-        this.removeImageDimensions(promoImage);
       }
     });
   }
@@ -89,27 +76,23 @@ export default class ForsonScreenshotFixes {
   }
 
   private adjustProductGridImages(): void {
-    const productGrid = this.document.querySelector('.grid.product-grid.grid--2-col-tablet-down.grid--4-col-desktop');
-    if (!productGrid) return;
-
-    const listItems = productGrid.querySelectorAll('li');
-    listItems.forEach(li => {
-      const mediaElement = li.querySelector('.media.media--hover-effect');
-      if (!(mediaElement instanceof HTMLElement)) return;
-
-      const images = mediaElement.querySelectorAll('img');
-      const targetImage = images.length >= 2 ? images[1] : images[0];
-      if (targetImage instanceof HTMLImageElement) {
-        targetImage.style.minHeight = '100%';
-      }
-    });
+    const productGrid = this.getElement('.grid.product-grid.grid--2-col-tablet-down.grid--4-col-desktop');
+    if (productGrid) {
+      this.getElements('li', productGrid).forEach(li => {
+        const mediaElement = li.querySelector('.media.media--hover-effect');
+        if (mediaElement instanceof HTMLElement) {
+          const images = mediaElement.querySelectorAll('img');
+          const targetImage = images.length >= 2 ? images[1] : images[0];
+          if (targetImage instanceof HTMLImageElement) {
+            targetImage.style.minHeight = '100%';
+          }
+        }
+      });
+    }
   }
 
   private removeSectionInterstitialHeight(): void {
-    const elements = this.document.querySelectorAll('.section-interstitial');
-    elements.forEach(element => {
-      if (!(element instanceof HTMLElement)) return;
-
+    this.getElements<HTMLElement>('.section-interstitial').forEach(element => {
       element.style.removeProperty('height');
       if (element.style.getPropertyPriority('height') === 'important') {
         element.style.setProperty('height', 'initial', 'important');
@@ -118,35 +101,26 @@ export default class ForsonScreenshotFixes {
   }
 
   private adjustImageStyles(): void {
-    const targetDivs = this.document.querySelectorAll('.image-wrap, .text-spacing');
+    this.getElements<HTMLElement>('.image-wrap, .text-spacing').forEach(div => {
+      div.style.removeProperty('height');
+      div.style.setProperty('padding-bottom', '1px', 'important');
 
-    targetDivs.forEach(div => {
-      if (div instanceof HTMLElement) {
-        div.style.removeProperty('height');
-        div.style.setProperty('padding-bottom', '1px', 'important');
-
-        const img = div.querySelector('img');
-        if (img instanceof HTMLImageElement) {
-          img.style.setProperty('height', 'auto', 'important');
-        }
+      const img = div.querySelector('img');
+      if (img instanceof HTMLImageElement) {
+        img.style.setProperty('height', 'auto', 'important');
       }
     });
   }
 
   private removeDisplayNone(): void {
     const classList = [
-      'jdgm-widget',
-      'jdgm-carousel',
-      'jdgm-carousel--default-theme',
-      'jdgm-carousel--done',
-      'jdgm-carousel-wrapper',
-      'jdgm-carousel__left-arrow',
-      'jdgm-carousel__right-arrow'
+      'jdgm-widget', 'jdgm-carousel', 'jdgm-carousel--default-theme',
+      'jdgm-carousel--done', 'jdgm-carousel-wrapper',
+      'jdgm-carousel__left-arrow', 'jdgm-carousel__right-arrow'
     ];
     
     classList.forEach(className => {
-      const elements = this.document.querySelectorAll(`.${className}`) as NodeListOf<HTMLElement>;
-      elements.forEach(element => {
+      this.getElements<HTMLElement>(`.${className}`).forEach(element => {
         element.style.removeProperty('display');
       });
     });
@@ -158,20 +132,26 @@ export default class ForsonScreenshotFixes {
   }
 
   private removeSectionHeight(): void {
-    const sectionElement = this.document.querySelector('.icons-with-text.section.section--full-width.section--padded-small.color-scheme.color-scheme--2');
-    if (sectionElement instanceof HTMLElement) {
+    const sectionElement = this.getElement<HTMLElement>('.icons-with-text.section.section--full-width.section--padded-small.color-scheme.color-scheme--2');
+    if (sectionElement) {
       sectionElement.style.removeProperty('height');
     }
   }
 
   private removeImgFitDimensions(): void {
-    const imgFitElements = this.document.querySelectorAll('.img-fit');
-    imgFitElements.forEach(element => {
-      if (element instanceof HTMLElement) {
-        element.style.removeProperty('height');
-        element.style.removeProperty('width');
-      }
+    this.getElements<HTMLElement>('.img-fit').forEach(element => {
+      element.style.removeProperty('height');
+      element.style.removeProperty('width');
     });
+  }
+
+  private applyResponsiveStyles(): void {
+    if (!this.isDesktopView()) {
+      const productIngredientsElement = this.getElement<HTMLElement>('.product-ingredients');
+      if (productIngredientsElement) {
+        productIngredientsElement.style.setProperty('--carousel-edge', 'max(calc(var(--spacing-edge) * 0), 0rem)');
+      }
+    }
   }
 
   private setImageDimensions(image: HTMLImageElement, parent: Element): void {
@@ -183,5 +163,17 @@ export default class ForsonScreenshotFixes {
   private removeImageDimensions(image: HTMLImageElement): void {
     image.style.removeProperty('min-width');
     image.style.removeProperty('min-height');
+  }
+
+  private isDesktopView(): boolean {
+    return this.document.body.clientWidth > 749;
+  }
+
+  private getElement<T extends Element>(selector: string, parent: Element | Document = this.document): T | null {
+    return parent.querySelector(selector) as T | null;
+  }
+
+  private getElements<T extends Element>(selector: string, parent: Element | Document = this.document): NodeListOf<T> {
+    return parent.querySelectorAll(selector) as NodeListOf<T>;
   }
 }
