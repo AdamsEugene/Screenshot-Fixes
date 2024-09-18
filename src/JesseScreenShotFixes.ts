29ea995ff8a4810d5d7c6280588042c4d983746c
amabange97 <amabangejesse@gmail.com>
2024-09-18 13:49:22 +0000

    fixed overlay issue complete IMF-7

---
+++ src/JesseScreenShotFixes.ts
@@ -0,0 +1,51 @@
+import { log } from "console";
+
+export default class JesseScreenShotFixes {
+  private document: Document;
+
+  /* constructor(dom: Document = document) {
+    this.document = dom;
+  } */
+
+  constructor(dom: Document = document) {
+    this.document = dom;
+    this.setupLoadEventListener();
+  }
+
+  private setupLoadEventListener(): void {
+    window.addEventListener("load", () => {
+      this.init();
+    });
+  }
+
+  public init(): void {
+    this.changeAfterBackgroundToTransparent();
+ 
+  }
+
+// Burga US Phone Cases.. fixes a loading overlay issue
+private changeAfterBackgroundToTransparent() {
+    const loadingBody = document.querySelector('.wbsk-ui__loading-body') as HTMLElement | null;
+  
+    if (loadingBody) {
+      const afterElementStyle = window.getComputedStyle(loadingBody, '::after');
+      
+      const currentBackgroundColor = afterElementStyle.getPropertyValue('background-color');
+  
+      if (currentBackgroundColor === 'rgba(0, 0, 0, 0.31)') {
+        const styleSheet = document.styleSheets[0]; 
+        const rule = `.wbsk-ui__loading-body::after { background: transparent !important; }`;
+        
+        styleSheet.insertRule(rule, styleSheet.cssRules.length); 
+  
+        console.log('Background changed to transparent');
+      } else {
+        console.log('Background does not match #0000004f');
+      }
+    } else {
+      console.log('Element with class "wbsk-ui__loading-body" not found');
+    }
+  }
+  
+
+}
