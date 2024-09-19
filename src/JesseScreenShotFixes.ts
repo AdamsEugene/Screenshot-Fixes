4edbbf2f576a804e117b4b1b3e098717afe4927d
amabange97 <amabangejesse@gmail.com>
2024-09-19 11:14:33 +0000

    fixed slider not showing on addison ross luxury

--- src/JesseScreenShotFixes.ts
+++ src/JesseScreenShotFixes.ts
@@ -4,6 +4,7 @@ export default class JesseScreenShotFixes extends Common {
   public init(containerId: string, debugMode: boolean): void {
     const func = () => {
       this.changeAfterBackgroundToTransparent();
+      this.updatePaddingForSmallScreens();
     };
     this.exec({ containerId, debugMode, func });
   }
@@ -30,4 +31,30 @@ export default class JesseScreenShotFixes extends Common {
       }
     }
   }
+
+//   Addison Ross Luxury
+  private updatePaddingForSmallScreens() {
+    // Define the media query for screens below 749px
+    const mediaQuery = this.iframeWindow.matchMedia("(max-width: 749px)");
+  
+    // Function to apply the padding-top change
+    const applyPaddingChange = () => {
+      if (mediaQuery.matches) {
+        // Add a new style rule to override the existing padding-top with !important
+        const styleSheet = this.dom.styleSheets[0]; // Select the first stylesheet
+        const rule = `
+          .featured-category .collection-list-wrapper.page-width {
+            padding-top: 250px !important;
+          }
+        `;
+        styleSheet.insertRule(rule, styleSheet.cssRules.length); // Inject the new rule
+        console.log("Padding-top changed to 250px for screens below 749px");
+      }
+    };
+  
+    // Apply the change on load
+    applyPaddingChange();
+  
+  }
+  
 }
