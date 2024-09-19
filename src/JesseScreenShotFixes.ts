ecb6eb23b197a30f637167055d8f761c4d43314f
adams eugene <adamseugene292@gmail.com>
2024-09-19 14:04:43 +0000

    updated

--- src/JesseScreenShotFixes.ts
+++ src/JesseScreenShotFixes.ts
@@ -11,47 +11,53 @@ export default class JesseScreenShotFixes extends Common {
 
   // Burga US Phone Cases.. fixes a loading overlay issue
   private changeAfterBackgroundToTransparent() {
-    const loadingBody = this.dom.querySelector(
-      ".wbsk-ui__loading-body"
-    ) as HTMLElement | null;
-    if (loadingBody) {
-      console.log(this.iframeWindow, "hey here i am");
-      console.log(this.dom, "hey");
-      const afterElementStyle = this.iframeWindow.getComputedStyle(
-        loadingBody,
-        "::after"
-      );
+    try {
+      const loadingBody = this.dom.querySelector(
+        ".wbsk-ui__loading-body"
+      ) as HTMLElement | null;
+      if (loadingBody) {
+        console.log(this.iframeWindow, "hey here i am");
+        console.log(this.dom, "hey");
+        const afterElementStyle = this.iframeWindow.getComputedStyle(
+          loadingBody,
+          "::after"
+        );
 
-      const currentBackgroundColor =
-        afterElementStyle.getPropertyValue("background-color");
-      if (currentBackgroundColor === "rgba(0, 0, 0, 0.31)") {
-        const styleSheet = this.dom.styleSheets[0];
-        const rule = `.wbsk-ui__loading-body::after { background: transparent !important; }`;
-        styleSheet.insertRule(rule, styleSheet.cssRules.length);
+        const currentBackgroundColor =
+          afterElementStyle.getPropertyValue("background-color");
+        if (currentBackgroundColor === "rgba(0, 0, 0, 0.31)") {
+          const styleSheet = this.dom?.styleSheets?.[0];
+          if (styleSheet) {
+            const rule = `.wbsk-ui__loading-body::after { background: transparent !important; }`;
+            styleSheet.insertRule(rule, styleSheet?.cssRules?.length);
+          }
+        }
       }
-    }
+    } catch (error) {}
   }
 
-//   Addison Ross Luxury
+  //   Addison Ross Luxury
   private updatePaddingForSmallScreens() {
     // Define the media query for screens below 749px
     const mediaQuery = this.iframeWindow.matchMedia("(max-width: 749px)");
-  
+
     // Function to apply the padding-top change
     const applyPaddingChange = () => {
-      if (mediaQuery.matches) {
-        // Add a new style rule to override the existing padding-top with !important
-        const styleSheet = this.dom.styleSheets[0]; // Select the first stylesheet
-        const rule = `
-          .featured-category .collection-list-wrapper.page-width {
-            padding-top: 250px !important;
+      try {
+        if (mediaQuery.matches) {
+          // Add a new style rule to override the existing padding-top with !important
+          const styleSheet = this.dom?.styleSheets?.[0]; // Select the first stylesheet
+          if (styleSheet) {
+            const rule = `
+            .featured-category .collection-list-wrapper.page-width {
+              padding-top: 250px !important;
+              }
+              `;
+            styleSheet.insertRule(rule, styleSheet?.cssRules?.length);
           }
-        `;
-        styleSheet.insertRule(rule, styleSheet.cssRules.length);
-      }
+        }
+      } catch (error) {}
     };
     applyPaddingChange();
-  
   }
-  
 }
