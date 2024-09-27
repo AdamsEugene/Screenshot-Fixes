ac280ec9dce562779c8a12824dd2ad3b5369abe2
sangryan <sangryan1@gmail.com>
2024-09-27 11:21:40 +0300

    Rubio Monocoat changes fix

--- src/RyanScreenShotFixes.ts
+++ src/RyanScreenShotFixes.ts
@@ -96,14 +96,22 @@ private setElementDisplayToBlock() {
 
   //RubioMonocoat
 private RubioMonocoatUpdateMenuState() {
-  const parentElement = this.dom.querySelector('.main-menu') as HTMLElement;
-  if (parentElement) {
-    const childElement = parentElement.querySelector('.main-menu__disclosure.has-motion') as HTMLElement;
-    if (childElement) {
-      childElement.classList.add('is-open');
-      childElement.setAttribute('open', '');
+  const parentElement1 = this.dom.querySelector('.main-menu') as HTMLElement;
+  if (parentElement1) {
+    const childElement1 = parentElement1.querySelector('.main-menu__disclosure.has-motion') as HTMLElement;
+    if (childElement1) {
+      childElement1.classList.add('is-open');
+      childElement1.setAttribute('open', '');
     }
   }
+
+  const parentElements2 = this.dom.querySelectorAll('.js-mega-nav') as NodeListOf<HTMLElement>;
+  parentElements2.forEach(parentElement2 => {
+    const detailsElements = parentElement2.querySelectorAll('details') as NodeListOf<HTMLDetailsElement>;
+    detailsElements.forEach(detailsElement => {
+      detailsElement.setAttribute('open', ''); 
+    });
+  });
 }
 
 }
