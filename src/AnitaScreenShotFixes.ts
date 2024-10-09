44c5a81adf42f85b638b9fd6571d44259e7a5158
Isaac Forson <youngtech303@gmail.com>
2024-10-09 16:29:22 +0000

    shot changes

--- src/AnitaScreenShotFixes.ts
+++ src/AnitaScreenShotFixes.ts
@@ -23,7 +23,7 @@ export default class AnitaScreenShotFixes {
     this.showGlobalOverlay();
     this.removeFixedPositionFromHeader();
     this.makeBackdropTransparent(); 
-    this.removeMainContentMargin();
+    // this.removeMainContentMargin();
   }
 
   private removeHeightProperty() {
@@ -135,17 +135,5 @@ export default class AnitaScreenShotFixes {
     }
   }
 
-  private removeMainContentMargin(): void {
-    const bodyElement = this.document.querySelector('body.product-mc-modified') as HTMLElement;
-  
-    if (bodyElement) {
-      const mainContentElement = bodyElement.querySelector('.main.content') as HTMLElement;
-      
-      if (mainContentElement) {
-        mainContentElement.style.setProperty('margin-top', '0', 'important');
-      }
-    }
-  }
-  
   
 }
