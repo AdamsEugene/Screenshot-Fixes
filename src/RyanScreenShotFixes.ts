540b98d844b4f358fc50988a321ae3527291daa4
sangryan <sangryan1@gmail.com>
2024-10-31 23:27:03 +0300

    Fix for Next adventure

--- src/RyanScreenShotFixes.ts
+++ src/RyanScreenShotFixes.ts
@@ -695,13 +695,16 @@ export default class RyanScreenshotFixes extends Common {
 
   //Next Adventure
   private NextAdventureSetSubmenuHeight() {
-    this.dom.querySelectorAll(".navmenu-item").forEach((parentElement: HTMLElement) => {
-        const submenu = parentElement.querySelector(".navmenu-submenu.navmenu-meganav");
-        if (submenu && (submenu as HTMLElement).style.height === "max-content") {
-            (submenu as HTMLElement).style.removeProperty("height");
-        }
-        (submenu as HTMLElement).style.setProperty("height", "revert-layer", "important");
-    });
+    try {
+        this.dom.querySelectorAll(".navmenu-item").forEach((parentElement: HTMLElement) => {
+            const submenu = parentElement.querySelector(".navmenu-submenu.navmenu-meganav") as HTMLElement | null;
+            
+            if (submenu) {
+                if (submenu.style.height === "max-content") submenu.style.removeProperty("height");
+                submenu.style.setProperty("height", "revert-layer", "important");
+            }
+        });
+    } catch {}
   }
 
   //be Rooted
