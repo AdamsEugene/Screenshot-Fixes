18828ee66a73fd77b70e8c2369d89fb9006a9daf
Benson Osei Mensah <kbenson643@gmail.com>
2025-01-02 12:17:50 +0000

    chore: setup benson fixes
    Signed-off-by: Benson Osei Mensah <kbenson643@gmail.com>

---
+++ src/BensonScreenShotFixes.ts
@@ -0,0 +1,11 @@
+import Common from "./Common";
+
+export default class BensonScreenshotFixes extends Common {
+  public init(containerId: string, debugMode: boolean): void {
+    const func = () => {
+     
+    };
+    this.exec({ containerId, debugMode, func });
+  }
+
+}
