a636dbddf17d9849d5a7d172a561704a183e9641
Benson Osei Mensah <kbenson643@gmail.com>
2025-01-02 14:11:31 +0000

    feat: add delay to DaisyLondonRemoveWidthFromSlickTrack function for improved rendering
    Signed-off-by: Benson Osei Mensah <kbenson643@gmail.com>

--- src/BensonScreenShotFixes.ts
+++ src/BensonScreenShotFixes.ts
@@ -3,14 +3,17 @@ import Common from "./Common";
 export default class BensonScreenshotFixes extends Common {
   public init(containerId: string, debugMode: boolean): void {
     const func = () => {
-      this.run(this.DaisyLondonRemoveWidthFromSlickTrack.bind(this));
+      this.DaisyLondonRemoveWidthFromSlickTrack.bind(this);
     };
     this.exec({ containerId, debugMode, func });
   }
 
-  private run(callback: Function, options: {
-    onError?: (error: Error) => void;
-  } = {}) {
+  private run(
+    callback: Function,
+    options: {
+      onError?: (error: Error) => void;
+    } = {}
+  ) {
     try {
       callback();
     } catch (error) {
@@ -20,17 +23,19 @@ export default class BensonScreenshotFixes extends Common {
 
   //Daisy London
   private DaisyLondonRemoveWidthFromSlickTrack() {
-    this.dom.querySelectorAll(".slick-list.draggable").forEach((parent) => {
-      const track = parent.querySelector(".slick-track") as HTMLElement;
-      if (track) {
-        track.style.width = "";
-        const currentSlide = track.querySelector(
-          ".slick-slide.slick-current"
-        ) as HTMLElement;
-        if (currentSlide) {
-          currentSlide.style.width = "100%";
+    setTimeout(() => {
+      this.dom.querySelectorAll(".slick-list.draggable").forEach((parent) => {
+        const track = parent.querySelector(".slick-track") as HTMLElement;
+        if (track) {
+          track.style.width = "";
+          const currentSlide = track.querySelector(
+            ".slick-slide.slick-current"
+          ) as HTMLElement;
+          if (currentSlide) {
+            currentSlide.style.width = "100%";
+          }
         }
-      }
-    });
+      });
+    }, 2000);
   }
 }
