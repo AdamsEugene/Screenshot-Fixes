14a49a8b0a0c92b1a3f1e55730b7af23592f9dfe
Attoh Jonathan <attohkweku@gmail.com>
2025-01-09 15:18:20 +0000

    init fixes build

--- src/JoeScreenShotFixes.ts
+++ src/JoeScreenShotFixes.ts
@@ -2,32 +2,37 @@ import Common from "./Common";
 
 export default class JoeScreenshotFixes extends Common {
     public init(containerId: string, debugMode: boolean): void {
-        const func = () => {};
+        const func = () => {
+            this.removeImagesWhiteOverlayRnnr()
+            this.removeBlackOverlayQueSkinCare()
+
+        };
         this.exec({ containerId, debugMode, func });
     }
 
     private removeImagesWhiteOverlayRnnr() {
-        try {
-            const elements = this.dom.querySelectorAll(
-                ".collection__loading"
-            ) as NodeListOf<HTMLElement>;
+        setTimeout(() => {
+            try {
+                const elements = this.dom.querySelectorAll(
+                    ".collection__loading"
+                ) as NodeListOf<HTMLElement>;
 
-            elements.forEach((el) => {
-                el.style.opacity = "0";
-            });
-        } catch (error) {}
+                elements.forEach((el) => {
+                    el.style.opacity = "0";
+                });
+            } catch (error) {}
+        }, 1000);
     }
 
-    private removeBlackOverlayQueSkinCare(){
+    private removeBlackOverlayQueSkinCare() {
+        try {
+            const modalBackdrop = this.dom.querySelector(
+                ".modal-backdrop.fade.show"
+            ) as HTMLElement;
 
-        try{
-
-            const modalBackdrop = this.dom.querySelector(".modal-backdrop.fade.show") as HTMLElement
-
-            if (modalBackdrop){
-                modalBackdrop.classList.remove("show")
+            if (modalBackdrop) {
+                modalBackdrop.classList.remove("show");
             }
-
-        }catch(error) {}
+        } catch (error) {}
     }
 }
