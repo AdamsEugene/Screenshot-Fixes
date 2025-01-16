bb3b3403ffc26f0507a88f248c2235e1a17a634a
ftechspace <fortunetede1@gmail.com>
2025-01-16 19:21:00 +0000

    adjust fixed position

--- src/FortuneScreenShotFixes.ts
+++ src/FortuneScreenShotFixes.ts
@@ -5,6 +5,7 @@ export default class FortuneScreenshotFixes extends Common {
   public init(containerId: string, debugMode: boolean): void {
     const func = () => {
       this.announcementBarTextOverlay();
+      this.adjustFixedElement();
     };
     this.exec({ containerId, debugMode, func });
   }
@@ -23,6 +24,27 @@ export default class FortuneScreenshotFixes extends Common {
         } catch (error) {
             return;
         }
+    }, delayTime);
+  }
+
+  private adjustFixedElement() {
+    setTimeout(() => {
+      try {
+        const bannerMediaDivs = this.dom.querySelectorAll('.banner__media') as NodeListOf<HTMLElement>;
+        bannerMediaDivs.forEach((div) => {
+          if (div.children.length === 1 && div.children[0].tagName.toLowerCase() === 'img') {
+            const img = div.children[0] as HTMLImageElement;
+            img.style.setProperty('top', '0px');
+          }
+        });
+        
+        const fixedElements = this.dom.querySelectorAll('.animate--fixed>img:not(.zoom):not(.deferred-media__poster-button), .animate--fixed>svg:not(.zoom):not(.deferred-media__poster-button)') as NodeListOf<HTMLElement>;
+        fixedElements.forEach((element) => {
+            element.style.setProperty('position', 'absolute', 'important');
+        });
+      } catch (error) {
+          return;
+      }
     }, delayTime);
   }
 }
