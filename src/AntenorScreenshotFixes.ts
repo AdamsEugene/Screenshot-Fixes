f5b82f7dcd3e4c3d5a4893454e2d6efc77ec056c
Antenor452 <nanaosei452@gmail.com>
2025-01-24 15:06:21 +0000

    A Pup Above - Removed pop up covering header and fixed the sizing for product slider

--- src/AntenorScreenshotFixes.ts
+++ src/AntenorScreenshotFixes.ts
@@ -3,30 +3,62 @@ import Common from "./Common";
 export default class AntenorScreenShotFixes extends Common {
   public init(containerId: string, debugMode: boolean): void {
     const func = () => {
-      this.setAPupAboveInactiveSlickSlideInititalOpacity();
+      this.aPupAboveScreenShotFixes();
       this.setNoodPromoCardImageSize();
     };
     this.exec({ containerId, debugMode, func });
   }
 
   //A pup above
+  private aPupAboveScreenShotFixes(): void {
+    try {
+      this.setAPupAboveInactiveSlickSlideInititalOpacity();
+      this.removeAPupAboveNutrientPopUp();
+      this.setAPupAboveProductSliderContainerHeight();
+    } catch (error) {}
+  }
   private setAPupAboveInactiveSlickSlideInititalOpacity(): void {
     try {
-      const nutrientSlickSlides = this.dom.querySelectorAll(
-        ".nutrients__slide.slick-slide"
-      ) as NodeListOf<HTMLElement>;
+      setInterval(() => {
+        const nutrientSlickSlides = this.dom.querySelectorAll(
+          ".nutrients__slide.slick-slide"
+        ) as NodeListOf<HTMLElement>;
 
-      nutrientSlickSlides.forEach((slide) => {
-        const activeClasses = ["slick-current", "slick-active"];
+        nutrientSlickSlides.forEach((slide) => {
+          const activeClasses = ["slick-current", "slick-active"];
 
-        const isSlideActive = activeClasses.every((className) =>
-          slide.classList.contains(className)
-        );
+          const isSlideActive = activeClasses.every((className) =>
+            slide.classList.contains(className)
+          );
 
-        if (!isSlideActive) {
-          slide.style.setProperty("opacity", "0");
-        }
-      });
+          if (!isSlideActive) {
+            slide.style.setProperty("opacity", "0");
+          }
+        });
+      }, 1000);
+    } catch (error) {}
+  }
+
+  private removeAPupAboveNutrientPopUp(): void {
+    try {
+      const popUpElement = this.dom.querySelector(
+        "#mobile-nutrient-pop-up"
+      ) as HTMLElement;
+      if (popUpElement) {
+        popUpElement.style.setProperty("display", "none");
+      }
+    } catch (error) {}
+  }
+
+  private setAPupAboveProductSliderContainerHeight(): void {
+    try {
+      const sliderContainer = this.dom.querySelector(
+        ".pdp-gallery.product-single__media-group.medium-up--one-half"
+      ) as HTMLElement;
+      if (sliderContainer) {
+        const actualHeight = sliderContainer.getAttribute("actualheight");
+        sliderContainer.style.setProperty("height", actualHeight);
+      }
     } catch (error) {}
   }
 
@@ -39,6 +71,7 @@ export default class AntenorScreenShotFixes extends Common {
 
       if (promoSlider) {
         promoSlider.style.setProperty("max-width", "100vw");
+        promoSlider.style.setProperty("height", "max-content");
 
         const divWithBGImage = promoSlider.querySelector(
           ".rimage-outer-wrapper.mobile-only.fade-in.lazyloaded"
